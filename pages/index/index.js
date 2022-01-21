f44506a7419b12dbaf837998ab876e5ad31438e3
Johnson Wu <wj_johnson9581314@163.com>
2022-01-21 14:16:35 +0800

    update demo

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,87 +1,109 @@
-// index.js
-// 获取应用实例
-// import drawQrcode from "weapp-qrcode-canvas-2d";
 const app = getApp()
-let ctx = null;
-let canvasCtx = null;
+let ctxWidth = '';
+let ctxHeight = '';
+let ctx = null; //Camera
+let canvaCtx=null;//canvas
 Page({
   data: {
-    url: ''
+ 
   },
-
-  onLoad() {
-    ctx = wx.createCameraContext();
-  },
-
-  onShow() {
-    this.changeText()
-  },
-
-  changeText: function () {
+  changeText() {
     wx.getSystemInfo({
-      success: (result) => {
-        console.log('result', result);
-      },
-    })
-  },
-
-  takePhoto: function () {
-    const that = this;
-    console.log('takePhoto');
-    ctx.takePhoto({
-      quality: 'heigh',
-      success: (res) => {
-        if (res.tempImagePath) {
-          that.tackCanvas(res.tempImagePath);
-        }
-
-
-      }
-    })
-  },
-  tackCanvas: function (path) {
-    const that = this;
-    wx.getImageInfo({
-      src: path,
-      success: (imageInfo) => {
-        console.log('imageInfo', imageInfo);
-        wx.createSelectorQuery().select('#image-canvas').fields({
-          node: true,
-          size: true
-        }).exec((canRes) => {
-          console.log('createSelectorQuery canRes', canRes)
-          const canvas = canRes[0].node;
-          canvasCtx = canvas.getContext('2d');
-          const img = canvas.createImage();
-          img.src = path;
-          
-          img.onload = () => { 
-            canvasCtx.drawImage(img, 0, 0);
-            canvasCtx.save();
-            wx.canvasToTempFilePath({
-              width: 260,
-              height: 150,
-              canvasId: 'canvasId',
-              fileType: 'jpg',
-              quality:'1',
-              canvas: canvas,
-              success: (canvasToPathResult) => {
-                console.log('canvasToPathResult', canvasToPathResult);
-                that.setData({
-                  url: canvasToPathResult.tempFilePath
-                })
-              }
-            })
-          }
-      
-          // canvasCtx.beginPath();
-          // canvasCtx.arc(260,150)
-          // canvasCtx.clip();
-
-          // canvasCtx.draw();
-         
+      success: res => {
+        let convasX = res.screenWidth / 4; //遮罩层上下的高度(生成canvas的起始横坐标)
+        let convasY = res.screenHeight / 5; //遮罩层左右的宽度(生成canvas的起始纵坐标)
+        let canvasWidth = convasX * 3; //中间裁剪部位的宽度
+        let canvasHeight = convasY * 3; //中间裁剪部位的高度
+        let convasXL = convasX / 2;
+        ctxWidth = canvasWidth; //canvas的宽度
+        ctxHeight = canvasHeight; //canvas的高度
+        this.setData({
+          info: res,
+          canvasWidth,
+          canvasHeight,
+          convasX,
+          convasXL,
+          convasY
         })
       }
     })
+  },
+  onLoad: function () {},
+  onShow() {
+    this.changeText()
+  },
+  takePhoto() {
+    ctx = wx.createCameraContext(); //初始化camera
+    ctx.takePhoto({ //生成图片
+      quality: 'high',
+      success: (res) => { //得到图片
+        this.takeCanvas(res.tempImagePath)
+        this.setData({
+          baseImg: res.tempImagePath,
+          isBaseImg: true,
+          isCanvas:true
+        })
+      }
+    })
+  },
+  takeCanvas(path) { //将拍摄的照片绘制到canvas上
+    wx.getImageInfo({
+      src: path,
+      success: imgInfo => {
+        let {info} = this.data;
+        let convasX = imgInfo.width / 4;
+        let convasY = imgInfo.height / 5;
+        let canvasWidth = convasX * 3;
+        let canvasHeight = convasY * 3;
+        let convasXL = convasX / 2;
+        // 我这里宽度和高度都计算了设备比，其实两个值是一样的 ，计算一个就够了
+        let prxHeight = info.windowHeight / imgInfo.height;//计算设备比
+        let prxWidth = info.windowWidth / imgInfo.width;//计算设备比
+        // ctx = wx.createCanvasContext("myCanvas", this);//自定义组件，需要加this
+        canvaCtx = wx.createCanvasContext("myCanvas");//自定义组件，需要加this
+        canvaCtx.drawImage(path,convasXL,(convasY-40),canvasWidth,canvasHeight,0,0,(parseInt(canvasWidth) * prxWidth),(parseInt(canvasHeight) * prxHeight));//绘制到canvas上的位置，canvas的宽高等
+        canvaCtx.draw(true, () => {
+          wx.canvasToTempFilePath({
+            fileType: "jpg",
+            quality: 0.3,
+            canvasId: "myCanvas",
+            success: canvasToPath => {
+              this.setData({
+                isSuccess:true,
+                isBaseImg: false,
+                isCanvas:false,
+                baseImg: canvasToPath.tempFilePath,
+                srcCanvasPath: canvasToPath.tempFilePath
+              })
+            }
+          })
+          // wx.canvasToTempFilePath({
+          //   fileType: "jpg",
+          //   quality: 0.3,
+          //   canvasId: "myCanvas",
+          //   success: canvasToPath => {
+          //     this.setData({
+          //       isBaseImg: false,
+          //       baseImg: canvasToPath.tempFilePath,
+          //       srcCanvasPath: canvasToPath.tempFilePath
+          //     })
+          //   }
+          // }, this)//自定义组件需要加this
+        })
+      }
+    })
+  },
+  clearPhoto(){
+    canvaCtx.clearRect(0,0,ctxWidth, ctxHeight)
+    canvaCtx.draw()
+    this.setData({
+        srcCanvasPath:"",
+        isCanvas:false,
+        isSuccess:false,
+        isBaseImg:false
+    })
+  },
+  confirmBack(){//确定绘制成功后的操作，根据自己的需求写即可
+ 
   }
 })
